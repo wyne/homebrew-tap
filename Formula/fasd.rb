fb3fc21059e3e7eec7043e474491b3477da34629
Justin Wyne <1986068+wyne@users.noreply.github.com>
2024-09-05 22:22:14 -0700

    fix fasd formula

--- Formula/fasd.rb
+++ Formula/fasd.rb
@@ -3,10 +3,15 @@ class Fasd < Formula
   homepage "https://github.com/wyne/fasd"
   url "https://github.com/wyne/fasd/archive/refs/tags/1.0.1.tar.gz"
   sha256 "d89cef0dbabb9891255d210146fc5699038e7d3ac9c0f872355689eb34d99974"
-  license "GPL-3.0-or-later"
+  license "MIT"
   head "https://github.com/wyne/fasd.git", branch: "main"
 
   def install
-    system "make", "install"
+    bin.install "fasd"
+    man1.install "fasd.1"
+  end
+
+  test do
+    system "#{bin}/fasd", "--init", "auto"
   end
 end
