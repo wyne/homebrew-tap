3c1ce7685bf296acfda978e85064feed9d1eaa09
Justin Wyne <1986068+wyne@users.noreply.github.com>
2024-09-08 01:53:33 -0700

    fasder v0.1.2

--- Formula/fasder.rb
+++ Formula/fasder.rb
@@ -1,8 +1,8 @@
 class Fasder < Formula
   desc "Modern version of fasd productivity booster, offering quick access to files and directories"
   homepage "https://github.com/wyne/fasder"
-  url "https://github.com/wyne/fasder/archive/refs/tags/0.1.1.tar.gz"
-  sha256 "79882f430500e8b455508fbcdf6803ed8336d0676d5f1aebbd031039b7ee61b6"
+  url "https://github.com/wyne/fasder/archive/refs/tags/0.1.2.tar.gz"
+  sha256 "809c79aa03392613090ad9bc6b6c05f377a1f6e7f32469b608f2b3ec9c3bd02e"
   license "MIT"
   head "https://github.com/wyne/fasder.git", branch: "main"
 
