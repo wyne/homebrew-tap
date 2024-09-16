2700d79695638eb9cf719a7450f52133d8122544
Justin Wyne <1986068+wyne@users.noreply.github.com>
2024-09-16 12:32:43 -0700

    fasder automation - download artifacts

--- fasder_update.sh
+++ fasder_update.sh
@@ -4,12 +4,30 @@
 REPO="wyne/fasder"
 TAP_FILE="Formula/fasder.rb"
 
+beta_flag=false
+
+# Parse options
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -b) beta_flag=true ;;
+        --beta) beta_flag=true ;;
+        *) echo "Unknown option: $1" ;;
+    esac
+    shift
+done
+
+# Check if the beta flag was set
+if [ "$beta_flag" = true ]; then
+    TAP_FILE="Formula/fasderbeta.rb"
+fi
+
 # Fetch the latest release version from GitHub API
 LATEST_VERSION=$(curl -s "https://api.github.com/repos/$REPO/releases/latest" | sed -n 's/.*"tag_name": "\(.*\)".*/\1/p')
 echo "Latest version: $LATEST_VERSION"
 
 # Download the latest release tarball
 TARBALL_URL="https://github.com/$REPO/archive/refs/tags/$LATEST_VERSION.tar.gz"
+TARBALL_URL="https://github.com/$REPO/releases/download/$LATEST_VERSION/fasder-$LATEST_VERSION.tar.gz"
 echo "Downloading tarball from $TARBALL_URL"
 curl -L -o "$LATEST_VERSION.tar.gz" "$TARBALL_URL"
 
