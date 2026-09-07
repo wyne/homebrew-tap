3a80df72611249ce4e4c5b829b4f23208a45982b
Justin Wyne <1986068+wyne@users.noreply.github.com>
2026-09-07 05:39:52 +0000

    fasder: add 0.1.7 bottle.

--- Formula/fasder.rb
+++ Formula/fasder.rb
@@ -6,6 +6,13 @@ class Fasder < Formula
   license "MIT"
   head "https://github.com/wyne/fasder.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/wyne/homebrew-tap/releases/download/fasder-0.1.7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c50755dac918a73d8152fe6cbd893a627b204076f893de4b6654b2e989d008ed"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6213bf9c79d4b352c01d635466c5198c24d0aea76de8fc14e1a7b671ac5059e6"
+    sha256 cellar: :any,                 x86_64_linux:  "4c65a4f1878e07823e3e9e491a22d786145b533381e88a192c5473dac3e0edac"
+  end
+
   depends_on "go" => :build
 
   def install
