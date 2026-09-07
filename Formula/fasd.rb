3d427c064aafe4676198ef1188497361ed425b98
Justin Wyne <1986068+wyne@users.noreply.github.com>
2026-09-07 05:39:52 +0000

    fasd: add 1.0.1 bottle.

--- Formula/fasd.rb
+++ Formula/fasd.rb
@@ -6,6 +6,14 @@ class Fasd < Formula
   license "MIT"
   head "https://github.com/wyne/fasd.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/wyne/homebrew-tap/releases/download/fasd-1.0.1"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ce3468c7dcb5f281b57a1241f68e7720da2da8528206c7b6fbf0afde5b30a64a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6bdb8fcbed9b392d39c8411c5d6f9feeb8fb23933619133e07ee45a1301111ae"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "268263f4b865f8ee608aa4a0745d730566c7c5ab0561619a6adf7f1354a39699"
+  end
+
   def install
     bin.install "fasd"
     man1.install "fasd.1"
